2379c8ef2e198b9d23ddbb6a2de45014c0368e1c
paul-cute <paulcuteflorin@protonmail.com>
2023-04-06 23:20:51 +0300

    row component

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'), 
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar')
   ],
 }
