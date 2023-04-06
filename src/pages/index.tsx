b8788950ab2e871c89c7ea3423040844b5313051
paul-cute <paulcuteflorin@protonmail.com>
2023-04-06 18:17:38 +0300

    types ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import requests from '../../utils/requests'
+import { Movie } from '../../typings'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({netflixOriginals}) {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated:Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+export default function Home({
+  netflixOriginals,
+  trendingNow,
+  topRated,
+  actionMovies,
+  comedyMovies,
+  horrorMovies,
+  romanceMovies,
+  documentaries
+}: Props) {
   console.log(netflixOriginals)
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
