1a38786e2a18a1dd129cba75898b9d9b6a8def7d
paul-cute <paulcuteflorin@protonmail.com>
2023-04-14 23:52:51 +0300

    Plans Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import useAuth from '../../hooks/useAuth'
 import { useRecoilValue } from 'recoil'
 import { modalState } from '../../atoms/modalAtom'
 import Modal from '../../components/Modal'
+import Plans from '../../components/Plans'
 
 
 interface Props {
@@ -34,9 +35,13 @@ export default function Home({
 
   const { loading } = useAuth();
 
-  const showModal = useRecoilValue(modalState)
+  const showModal = useRecoilValue(modalState);
+  const subscription = false;
 
-  if(loading) return null;
+
+  if(loading || subscription === null) return null;
+
+  if(!subscription) return <Plans/>
 
   return (
     <div  className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
